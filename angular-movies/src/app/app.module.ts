9adf7da32d5924c72f1694cf63baf7b22b35cbd3
senaid123 <senaid968@gmail.com>
2023-05-06 17:30:28 +0200

    generate material module and add toolbar-module

--- angular-movies/src/app/app.module.ts
+++ angular-movies/src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppComponent } from './app.component';
 import { MoviesListComponent } from './movies/movies-list/movies-list.component';
 import { GenericListComponent } from './utilities/generic-list/generic-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MaterialModule } from './material/material.module';
+import { MenuComponent } from './menu/menu.component';
 @NgModule({
   declarations: [
     AppComponent,
     MoviesListComponent,
-    GenericListComponent
+    GenericListComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
